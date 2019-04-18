e05149542ebd87f9d056b5c617e5d9ae63d396db
Rhys Hudson <rhyshudsondotcom@gmail.com>
2019-04-18 07:47:15 +0000

    Began Sub Encryption function

--- main.c
+++ main.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 //Function Prototypes listed here.
-char *a_RotEncrypt(char *message, int key);
-char *b_RotDecrypt(char *message, int key);
+char *RotEncrypt(char *message, int Rotkey);
+char *RotDecrypt(char *message, int Rotkey);
+char *SubEncrypt(char *message, char *Cipherkey);
+
 int main() {
-    char ui = 'b';
-    char message[100] = "DMBQXOS SGHR LDRRZFD";
-	int key = 25;
+    char ui = 'c';
+    char message[] = "PLEASE GET MILK AT THE SHOPS";
+	int Rotkey = 4;
+	char Cipherkey[] = "QWERTYUIOPASDFGHJKLZXCVBNM" ;
 	
     switch(ui){
         case 'a' : 
             printf("Message: %s\n", message);
-            printf("Encrypted Message: %s\n", a_RotEncrypt(message, key));
+            printf("Encrypted Message: %s\n", RotEncrypt(message, Rotkey));
             break;
         case 'b' :
             printf("Encrypted Message: %s\n", message);
-            printf("Decrypted Message: %s\n", b_RotDecrypt(message, key));
+            printf("Decrypted Message: %s\n", RotDecrypt(message, Rotkey));
+            break;
+        case 'c' :
+            printf("message: %s\n", message);
+            printf("Encrypted Message: %s\n", SubEncrypt(message, Cipherkey));
             break;
         default : printf("Unknown Option %c\nPlease enter a, b, c, d, e, f.\n", ui); 
     }	
@@ -22,20 +30,19 @@ int main() {
   return 0;
 }
 
-char *a_RotEncrypt(char *message, int key){
+char *RotEncrypt(char *message, int Rotkey){
     char letter;
-    int i;
-    for(i = 0; message[i] != '\0'; ++i){
+    for(int i = 0; message[i] != '\0'; i++){
         letter = message[i];
         if(letter >= 'a' && letter <= 'z'){
-            letter = letter + key;
+            letter = letter + Rotkey;
             if(letter > 'z'){
                 letter = letter - 'z' + 'a' - 1;
             }
             message [i] = letter;
         }
         else if(letter >= 'A' && letter <= 'Z'){
-            letter = letter + key;
+            letter = letter + Rotkey;
             if(letter > 'Z'){
                 letter = letter - 'Z' + 'A' - 1;
             }
@@ -44,13 +51,12 @@ char *a_RotEncrypt(char *message, int key){
     }
     return message;
 }
-char *b_RotDecrypt(char *message, int key){
+char *RotDecrypt(char *message, int Rotkey){
     char letter;
-    int i;
-    for(i = 0; message[i] != '\0'; ++i){
+    for(int i = 0; message[i] != '\0'; i++){
 		letter = message[i];
 		if(letter >= 'a' && letter <= 'z'){
-			letter = letter - key;
+			letter = letter - Rotkey;
 			
 			if(letter < 'a'){
 				letter = letter + 'z' - 'a' + 1;
@@ -58,12 +64,28 @@ char *b_RotDecrypt(char *message, int key){
 			message[i] = letter;
 		}
 		else if(letter >= 'A' && letter <= 'Z'){
-			letter = letter - key;
+			letter = letter - Rotkey;
 			if(letter < 'A'){
 				letter = letter + 'Z' - 'A' + 1;
 			}
 			message[i] = letter;
 		}
 	}
+    return message;
+}
+char *SubEncrypt(char *message, char *Cipherkey){
+    int keyindex;
+    for(int i = 0; message[i] != '\0'; i++){
+        keyindex = message[i];
+        if(keyindex >= 'A' && keyindex <= 'Z'){
+            message[i] = Cipherkey[keyindex];
+        }
+        if(keyindex >= 'a' && keyindex <= 'z'){
+            message[i] = Cipherkey[keyindex];
+        }
+        else {
+            message[i] = message[i];
+        }
+    }
     return message;
 }
