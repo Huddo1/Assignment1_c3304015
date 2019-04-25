70b401404bbc97a84e69f66963f187e4ebd73415
Rhys Hudson <rhyshudsondotcom@gmail.com>
2019-04-25 05:58:44 +0000

    Completion of Substitution Decryption with a cipher key.

--- main.c
+++ main.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
-#include <string.h>
-//Function Prototypes listed here.
+#include <ctype.h>
+
+//FUNCTION PROTOTYPES
 char *RotEncrypt(char *message, int Rotkey);
 char *RotDecrypt(char *message, int Rotkey);
-char *SubEncrypt(char *message, char *Cipherkey);
+char *SubEncrypt(char *message, char *CipherKey);
+char *SubDecrypt(char *message, char *CipherKey);
 
+//MAIN
 int main() {
-    char ui = 'c';
-    char message[] = "PLEASE GET MILK AT THE SHOPS";
-	int Rotkey = 4;
-	char Cipherkey[] = "QWERTYUIOPASDFGHJKLZXCVBNM" ;
+    char ui = 'd';
+    char message[] = "qZzQEA QZ LXFKOLT";
+	int Rotkey = 25;  
+	char CipherKey[] = "QWERTYUIOPASDFGHJKLZXCVBNM";
 	
     switch(ui){
         case 'a' : 
@@ -21,27 +24,25 @@ int main() {
             printf("Decrypted Message: %s\n", RotDecrypt(message, Rotkey));
             break;
         case 'c' :
-            printf("message: %s\n", message);
-            printf("Encrypted Message: %s\n", SubEncrypt(message, Cipherkey));
+            printf("Message: %s\n", message);
+            printf("Encrypted Message: %s\n", SubEncrypt(message, CipherKey));
+            break;
+        case 'd' :
+            printf("Encrypted Message: %s\n", message);
+            printf("Decrypted Message: %s\n", SubDecrypt(message, CipherKey));
             break;
         default : printf("Unknown Option %c\nPlease enter a, b, c, d, e, f.\n", ui); 
     }	
 	
   return 0;
 }
-
+//FUNCTION DEFINITIONS
+//Rotation encryption function:
 char *RotEncrypt(char *message, int Rotkey){
     char letter;
     for(int i = 0; message[i] != '\0'; i++){
-        letter = message[i];
-        if(letter >= 'a' && letter <= 'z'){
-            letter = letter + Rotkey;
-            if(letter > 'z'){
-                letter = letter - 'z' + 'a' - 1;
-            }
-            message [i] = letter;
-        }
-        else if(letter >= 'A' && letter <= 'Z'){
+        letter = toupper(message[i]);
+        if(letter >= 'A' && letter <= 'Z'){
             letter = letter + Rotkey;
             if(letter > 'Z'){
                 letter = letter - 'Z' + 'A' - 1;
@@ -51,19 +52,12 @@ char *RotEncrypt(char *message, int Rotkey){
     }
     return message;
 }
+//Rotation decryption function:
 char *RotDecrypt(char *message, int Rotkey){
     char letter;
     for(int i = 0; message[i] != '\0'; i++){
-		letter = message[i];
-		if(letter >= 'a' && letter <= 'z'){
-			letter = letter - Rotkey;
-			
-			if(letter < 'a'){
-				letter = letter + 'z' - 'a' + 1;
-			}
-			message[i] = letter;
-		}
-		else if(letter >= 'A' && letter <= 'Z'){
+		letter = toupper(message[i]);
+		if(letter >= 'A' && letter <= 'Z'){
 			letter = letter - Rotkey;
 			if(letter < 'A'){
 				letter = letter + 'Z' - 'A' + 1;
@@ -73,19 +67,39 @@ char *RotDecrypt(char *message, int Rotkey){
 	}
     return message;
 }
-char *SubEncrypt(char *message, char *Cipherkey){
-    int keyindex;
+//Substitution encryption function:
+char *SubEncrypt(char *message, char *CipherKey){
+  int message_index;
+  for(int i = 0; message[i] != '\0'; i++){
+    message_index = toupper(message[i]) - 'A';
+    if(message_index >= 0 && message_index < 26){
+      message[i] = CipherKey[message_index];
+    }
+    else{
+      message[i] = message[i];
+    }
+  }
+  return message;
+}
+//Substitution decryption function:
+char *SubDecrypt(char *message, char *CipherKey){
+    int message_index, cipher_index;
+    char letter;
     for(int i = 0; message[i] != '\0'; i++){
-        keyindex = message[i];
-        if(keyindex >= 'A' && keyindex <= 'Z'){
-            message[i] = Cipherkey[keyindex];
+        message_index = toupper(message[i]) - 'A';
+        if(message_index >= 0 && message_index < 26){
+            letter = toupper(message[i]);
+            for(int j = 0; j <26; j++){
+                if(CipherKey[j] == letter){
+                     cipher_index = j;
+                }
+            }
+            message[i] = 'A' + cipher_index;
         }
-        if(keyindex >= 'a' && keyindex <= 'z'){
-            message[i] = Cipherkey[keyindex];
-        }
-        else {
+        else{
             message[i] = message[i];
         }
     }
     return message;
 }
+
